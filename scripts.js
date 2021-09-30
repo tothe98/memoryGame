509fe5395a772965c0f473f72bdc6199bd018a57
SilverPc98 <totherikjanos@gmail.com>
2021-09-30 12:53:51 +0200

    Done projectt

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-const cards = document.querySelectorAll('.memory-card');
+const root = document.querySelector('.memory-game');
 const counterElement = document.querySelector('#tryingCounter');
 const endInterface = document.querySelector('.end-interface');
 const secondLbl = document.querySelector('#second');
 const minutesLbl = document.querySelector('#minutes');
-const SCORE = 6;
+const MAXLEVEL = 3;
 
+let SCORE = 2;
+let cards = null;
 let hasFlippedCard = false;
 let lockBoard = true;
 let firstCard, secondCard;
@@ -13,11 +15,130 @@ let scoreCounter = 0;
 let sec = 0;
 let minutes = 0;
 let second = 0;
+let firstClick = true;
+let level = 1;
+
+let state = [];
+
+function fetchAndRender() {
+    state = JSON.parse(`
+    [
+        {
+            "id": "vue",
+            "firstCard": "img/vue.svg",
+            "secondCard": "img/vue.svg",
+            "alt": "Vue programming language",
+            "level":1
+        },
+        {
+            "id": "angular",
+            "firstCard": "img/angular.svg",
+            "secondCard": "img/angular.svg",
+            "alt": "Vue programming language",
+            "level":1
+        },
+        {
+            "id": "ember",
+            "firstCard": "img/ember.svg",
+            "secondCard": "img/ember.svg",
+            "alt": "Vue programming language",
+            "level":2
+        },
+        {
+            "id": "aurelia",
+            "firstCard": "img/aurelia.svg",
+            "secondCard": "img/aurelia.svg",
+            "alt": "Vue programming language",
+            "level":2
+        },
+        {
+            "id": "backbone",
+            "firstCard": "img/backbone.svg",
+            "secondCard": "img/backbone.svg",
+            "alt": "Vue programming language",
+            "level":2
+        },
+        {
+            "id": "react",
+            "firstCard": "img/react.svg",
+            "secondCard": "img/react.svg",
+            "alt": "Vue programming language",
+            "level":2
+        },
+        {
+            "id": "angular",
+            "firstCard": "img/angular.svg",
+            "secondCard": "/angular.svg",
+            "alt": "Vue programming language",
+            "level":3
+        },
+        {
+            "id": "angular",
+            "firstCard": "img/angular.svg",
+            "secondCard": "/angular.svg",
+            "alt": "Vue programming language",
+            "level":3
+        },
+        {
+            "id": "angular",
+            "firstCard": "img/angular.svg",
+            "secondCard": "/angular.svg",
+            "alt": "Vue programming language",
+            "level":3
+        },
+        {
+            "id": "angular",
+            "firstCard": "img/angular.svg",
+            "secondCard": "/angular.svg",
+            "alt": "Vue programming language",
+            "level":3
+        },
+        {
+            "id": "angular",
+            "firstCard": "img/angular.svg",
+            "secondCard": "/angular.svg",
+            "alt": "Vue programming language",
+            "level":3
+        },
+        {
+            "id": "angular",
+            "firstCard": "img/angular.svg",
+            "secondCard": "/angular.svg",
+            "alt": "Vue programming language",
+            "level":3
+        }
+    ]
+    `);
+    let html = "";
+    let tmpCounter = 0;
+    state.forEach(element => {
+        if (element.level === level) {
+            tmpCounter++;
+            html += `
+        <div class="memory-card" data-framework="${element.id}">
+            <img src="${element.firstCard}" alt="${element.alt}" class="front-face">
+            <img src="img/backFace.png" alt="backFace logó" class="back-face">
+        </div>
+        <div class="memory-card" data-framework="${element.id}">
+            <img src="${element.secondCard}" alt="${element.alt}" class="front-face">
+            <img src="img/backFace.png" alt="backFace logó" class="back-face">
+        </div>
+        `;
+        }
+    });
+    root.innerHTML = html;
+    SCORE = tmpCounter;
+    cards = document.querySelectorAll('.memory-card');
+    cards.forEach(card => card.addEventListener('click', flipCard));
+    shuffle();
+
+}
 
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
     this.classList.toggle('flip');
+    if (firstClick) { startTimer(); firstClick = false }
 
     if (!hasFlippedCard) {
         //első kártya
@@ -48,16 +169,24 @@ function disableCards() {
     resetBoard();
     if (scoreCounter === SCORE) {
         setTimeout(() => {
-            myStopFunction();
-            document.querySelector('#time').innerHTML = minutes+" perc " + second+" másodperc"
-            document.querySelector('#tryingCount').innerHTML = counter;
-            endInterface.style.display = "flex";
+            if(level === MAXLEVEL){
+                myStopFunction();
+                document.querySelector('#time').innerHTML = minutes + " perc " + second + " másodperc"
+                document.querySelector('#tryingCount').innerHTML = counter;
+                endInterface.style.display = "flex";
+            } else {
+                level++;
+                scoreCounter = 0;
+                document.querySelector('#level').innerHTML = level;
+                fetchAndRender();
+            }
         }, 1500); //1.5 sec
     }
 }
 
 function unflipCards() {
     lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
@@ -83,8 +212,9 @@ function reset() {
     counter = 0;
     scoreCounter = 0;
     counterElement.innerHTML = counter;
-    shuffle();
-    startTimer();
+    level = 1;
+    firstClick = true;
+    fetchAndRender();
 }
 
 function shuffle() {
@@ -93,20 +223,20 @@ function shuffle() {
         card.style.order = randomPos;
     });
 }
-(shuffle());
 
 function start() {
     const box = document.querySelector(".start-interface");
-    cards.forEach(card => card.addEventListener('click', flipCard));
+    document.querySelector('#level').innerHTML = level;
     hasFlippedCard = false;
     lockBoard = false;
     firstCard = null;
     secondCard = null;
     counter = 0;
     scoreCounter = 0;
-    shuffle();
     box.style.display = "none";
-    startTimer();
+    level = 1;
+    firstClick = true;
+    fetchAndRender();
 }
 
 function restart() {
@@ -122,9 +252,19 @@ function restart() {
     counter = 0;
     counterElement.innerHTML = counter;
     scoreCounter = 0;
-    shuffle();
     box.style.display = "none";
-    startTimer();
+    firstClick = true;
+    level = 1;
+    fetchAndRender();
+    
+    shuffle();
+}
+
+function stop() {
+    myStopFunction();
+    const box = document.querySelector(".start-interface");
+    box.style.display = "flex";
+
 }
 
 function pad(val) {
@@ -142,6 +282,6 @@ function startTimer() {
 }
 
 function myStopFunction() {
-    sec=0;
+    sec = 0;
     clearInterval(myInterval);
 }
